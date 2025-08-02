19d6e55208d91e3367c321faf0fc35623aca1c2f
Patiphat <youaremyhopes@gmail.com>
2025-08-03 00:41:05 +0700

    dreams

--- index.js
+++ index.js
@@ -14,9 +14,33 @@ var product = [{
     type: 'shirt'
 } , {
     id: 3,
-    img: 'https://images.unsplash.com/photo-1511746315387-c4a76990fdce?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    name: 'Adidas Shirt',
-    price: 1500,
-    description: 'Adidas shirt Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ipsum non repellendus earum libero reprehenderit.',
-    type: 'shirt'
-}]
+    img: 'https://images.unsplash.com/photo-1611510338559-2f463335092c?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Nike Shoe',
+    price: 5000,
+    description: 'Nike Shoe Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ipsum non repellendus earum libero reprehenderit.',
+    type: 'shoe'
+}];
+
+
+$(document).ready(() => {
+    var html = '';
+    for (let i = 0 ; i < product.length ; i++) {
+        html += `<div class="product-items ${product[i].type}">
+                    <img src="${product[i].img}" alt="">
+                    <p class="product-name">${product[i].name}</p>
+                    <p class="product-price">${numberWithCommas(product[i].price)} THB</p>
+                </div>` 
+    }
+    $("#productlist").html(html);
+})
+
+
+
+
+function numberWithCommas(x) {
+    x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x))
+        x = x.replace(pattern, "$1,$2");
+    return x;
+}
