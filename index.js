bbb2612c2cb6d1ba0e7df37d625b2fcc9d4dfd85
Patiphat <youaremyhopes@gmail.com>
2025-08-02 23:45:28 +0700

    dreams

---
+++ index.js
@@ -0,0 +1,7 @@
+var product = [{
+    id: 1,
+    img: 'https://images.unsplash.com/photo-1592750475338-74b7b21085ab?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Iphone',
+    price: 16000,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ipsum non repellendus earum libero reprehenderit.'
+} , {} , {}]
