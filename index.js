76cdbaf45d454e81b959ef4f4bf005fbafee2d35
Patiphat <youaremyhopes@gmail.com>
2025-08-02 23:59:48 +0700

    dreams

--- index.js
+++ index.js
@@ -3,5 +3,20 @@ var product = [{
     img: 'https://images.unsplash.com/photo-1592750475338-74b7b21085ab?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     name: 'Iphone',
     price: 16000,
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ipsum non repellendus earum libero reprehenderit.'
-} , {} , {}]
+    description: 'Iphone Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ipsum non repellendus earum libero reprehenderit.',
+    type: 'phone'
+} , {
+    id: 2,
+    img: 'https://images.unsplash.com/photo-1511746315387-c4a76990fdce?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Adidas Shirt',
+    price: 1500,
+    description: 'Adidas shirt Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ipsum non repellendus earum libero reprehenderit.',
+    type: 'shirt'
+} , {
+    id: 3,
+    img: 'https://images.unsplash.com/photo-1511746315387-c4a76990fdce?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    name: 'Adidas Shirt',
+    price: 1500,
+    description: 'Adidas shirt Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus ipsum non repellendus earum libero reprehenderit.',
+    type: 'shirt'
+}]
