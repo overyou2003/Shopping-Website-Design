afc78fbe9b424cf4a7e4c9ca8d210836c4c21a5b
Patiphat <youaremyhopes@gmail.com>
2025-08-03 17:06:26 +0700

    dreams

--- index.js
+++ index.js
@@ -184,6 +184,7 @@ function plusandminus(action , index) {
                 cancelButtonText: 'Cancel'
                 })
             }
+            
         }
     }
     else if(action == '+') {
